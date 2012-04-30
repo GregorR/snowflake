0b79871db40c383255fe71c15b65760b32cffde5
Gregor Richards <Richards@codu.org>
2012-04-30 17:23:06 -0400

    Some scripts for smart bootstrapping.
    --HG--
    rename : core/buildroot.sh => core/bootstraproot.sh
    rename : core/buildroot.sh => core/enter-chroot-prime.sh
    rename : core/buildroot.sh => core/enter-chroot.sh

--- core/config/bootstrap.sh
+++ core/config/bootstrap.sh
@@ -2,22 +2,28 @@
 set -e
 
 # make
-cd make-MAKE_VERSION
+cd /src/make-MAKE_VERSION
 with gcc -q make/MAKE_VERSION -- env CFLAGS='-O2 -g -D__BEOS__' \
     sh -c './configure --prefix=/usr && ./build.sh && ./make && ./make install'
 cd ..
 
 # gawk
-cd gawk-GAWK_VERSION
+cd /src/gawk-GAWK_VERSION
 with gcc make -q gawk/GAWK_VERSION -- sh -c './configure --prefix=/usr && make && make install && ln -fs gawk /usr/bin/awk'
-cd ..
 
 # pkgsrc
-if [ -e pkgsrc ] ; then mv pkgsrc /var/ ; fi
+[ -e /src/pkgsrc ] && mv /src/pkgsrc /var/
 if [ -e /var/pkgsrc ]
 then
     cd /var/pkgsrc/bootstrap
     with gcc make gawk -q pkgsrc/PKGSRC_VERSION -- env \
         CC='gcc -D_GNU_SOURCE -D_BSD_SOURCE' USE_NATIVE_GCC=yes NOGCCERROR=yes \
         ./bootstrap --prefix=/usr --varbase=/var
+    echo gcc gawk > /pkg/pkgsrc/PKGSRC_VERSION/deps
+    with -q pkgsrc/PKGSRC_VERSION -- echo -e 'CC=gcc -D_GNU_SOURCE -D_BSD_SOURCE\nUSE_NATIVE_GCC=yes\nNOGCCERROR=yes' \
+        >> /etc/mk.conf
 fi
+
+# delete the source
+cd /
+rm -rf /src/

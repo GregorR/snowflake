7e72ced9876e208d926fe0c237bc20f30cd4323e
Gregor Richards <Richards@codu.org>
2012-11-13 02:24:21 -0500

    Updates to pkgsrc-tests.

--- core/pkgsrc-tests/breakdownwiki.sh
+++ core/pkgsrc-tests/breakdownwiki.sh
@@ -11,7 +11,7 @@ MUSL_V="$4"
 
 printf '%s\n' 'This is an automatically-generated report of NetBSD pkgsrc build results from [[http://bitbucket.org/GregorR/snowflake|Snowflake]] revision '"$SF_REV"', using musl '"$MUSL_V"'. The purpose of this report is to give a general idea of what packages do and do not build on musl. When possible, you should refer to manually created build instructions rather than this automatically generated table.
 
-Each row indicates whether the dependencies for a package built, whether the package itself built, whether the package passed its tests, and any patches against pkgsrc which were necessary to build the package. Please note that the pkgsrc tests build '\'\''every'\'\'' package with the CFLAGS -D_GNU_SOURCE -D_BSD_SOURCE, so information on whether they are needed will be lost.
+Each row indicates whether the dependencies for a package built, whether the package itself built, whether the package passed its tests, and any patches against pkgsrc which were necessary to build the package.
 
 If you have patches to make other packages build, please report them on the [[https://bitbucket.org/GregorR/musl-pkgsrc-patches/issues|musl-pkgsrc-patches issue tracker]].
 
